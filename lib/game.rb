a6d27813c566d39026fda6ac7d717b1913187f0d
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-06-08 18:24:54 -0700

    Add 12 turns_left, increment down per guess

--- lib/game.rb
+++ lib/game.rb
@@ -18,10 +18,16 @@ class Game
     @secret_code = determine_secret_code
     p @secret_code
     # Game loop
-    
-    guess = player.make_guess
-    feedback = provide_feedback(guess)
-    puts "Correct Positions: #{feedback[0]} | Correct Digits: #{feedback[1]}"
+    turns_left = 12
+
+    loop do
+      guess = player.make_guess
+      feedback = provide_feedback(guess)
+      puts "Correct Positions: #{feedback[0]} | Correct Digits: #{feedback[1]}"
+      turns_left -= 1
+
+      break if turns_left == 0
+    end
 
   end
 
